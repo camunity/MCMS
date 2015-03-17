1029ee0c3e578e7716d5c709c5343d0c8ba219d7
kbendelow <kbendelow@me.com>
2015-03-17 17:40:06 -0500

    is there something wrong?

---
+++ MCMS/GameViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameViewController.h
+//  MCMS
+//
+//  Created by Kyle on 3/17/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
---
+++ MCMS/GameViewController.m
@@ -0,0 +1,25 @@
+//
+//  GameViewController.m
+//  MCMS
+//
+//  Created by Kyle on 3/17/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import "GameViewController.h"
+
+
+@interface GameViewController ()
+
+@end
+
+@implementation GameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+@end

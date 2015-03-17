b627338105a8783aca5c39f2ce6a43c062a0973e
camunity <cameron.flowers13@gmail.com>
2015-03-17 15:49:45 -0500

    testing edit button

--- MCMS/DetailViewController.m
+++ MCMS/DetailViewController.m
@@ -9,29 +9,55 @@
 #import "DetailViewController.h"
 #import "MagicalCreature.h"
 
-@interface DetailViewController ()
+@interface DetailViewController () <UITableViewDelegate, UITableViewDataSource >
 @property (strong, nonatomic) IBOutlet UILabel *creatureNameLabel;
 @property (strong, nonatomic) IBOutlet UILabel *creatureDetailLabel;
 @property (strong, nonatomic) IBOutlet UITextField *creatureNameTextField;
 @property (strong, nonatomic) IBOutlet UITextField *creatureDetailTextField;
-
+@property (strong, nonatomic) IBOutlet UITableView *accessoriesTableView;
+@property NSArray *weaponsAvailable;
+@property (strong, nonatomic) IBOutlet UIButton *editButton;
 @end
 
 @implementation DetailViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.weaponsAvailable = [NSArray arrayWithObjects:
+                             @"Hairball",
+                             @"Nunchucks",
+                             @"Urine Stream",
+                             @"A Mother's Love"
+                             @"Crossbow",
+                             @"Ben Beard",
+                             nil];
     // Do any additional setup after loading the view.
 }
 
 
 
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"WeaponsCell"];
+    cell.textLabel.text = [self.weaponsAvailable objectAtIndex:indexPath.row];
+    return cell;
+}
 
 
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.weaponsAvailable.count;
+}
 
-
-- (IBAction)onEditButtonPressed:(id)sender {
+- (IBAction)onEditButtonPressed:(UIButton *)sender {
     
+    if ([sender.titleLabel.text isEqualToString:@"Edit"]) {
+        sender.titleLabel.text = @"Done";
+        [self.accessoriesTableView setEditing:YES animated:YES];
+    } else {
+        sender.titleLabel.text = @"Edit";
+        [self.accessoriesTableView setEditing:NO animated:YES];
+    }
 
 
 }
